1518553c3084559775ce44890815c73934c349d7
Lehti <leaf.mmo@gmail.com>
2023-10-05 05:10:54 +0900

    たまに回転

--- react-app/src/MainImages.tsx
+++ react-app/src/MainImages.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './MainImages.scss';
 import ModalImage from "./ModalImage"
 
@@ -7,7 +7,7 @@ const MainImages = () => {
   for (let i = 1; i <= 20; i++) {
     items.push(`${process.env.PUBLIC_URL}/images/main/IMG_${i}.webp`);
   }
-  
+
   const [imageUrl, setImageUrl] = useState('');
   const getImageUrl = (event: React.MouseEvent<HTMLElement>) => {
     const image_url = event.currentTarget.getAttribute("src") || '';
@@ -17,10 +17,23 @@ const MainImages = () => {
     setImageUrl('');
   }
 
+  const main_images = useRef<HTMLDivElement>(null);
+  const initClassName: string = "main__image";
+  setInterval(() => {
+    const n: number = Math.floor(Math.random() * items.length);
+    const el = main_images.current?.children[n];
+    if (el) {
+      el.className = `${initClassName} rotate__image`;
+      setTimeout(() => {
+        el.className = initClassName
+      }, 2000);
+    }
+  }, 15000);
+
   return (
     <div className='main__images__wrapper'>
-      <div className='main__images'>
-        {items.map((item, i) => <img className="main__image" src={item} alt="main-image" key={i} onClick={getImageUrl}/>)}
+      <div className='main__images' ref={main_images}>
+        {items.map((item, i) => <img className="main__image" src={item} alt="main-image" key={i} onClick={getImageUrl} />)}
       </div>
       {/* { imageUrl !== '' &&  <ModalImage image_url={imageUrl} closeModal={closeModal}/> } */}
     </div>
